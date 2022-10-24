e161b548a06ee284153dbb281dca5b055f751b5b
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-10-24 21:45:30 +0200

    Added routes and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from '../styles/Header.module.css'
+import { Link, Route, Routes } from 'react-router-dom';
 
 function Header() {
     return (
-        <div>Header</div>
+        <header className={styles.header} >
+            <div>Tres Belle</div>
+            <nav className={styles.navbar}>
+                <div><Link to="/">Home</Link></div>
+                <div><Link to="/posters">Posters</Link></div>
+                <div><Link to="/login">Login</Link></div>
+            </nav>
+        </header>
     )
 }
 
-export default Header
+export default Header;
