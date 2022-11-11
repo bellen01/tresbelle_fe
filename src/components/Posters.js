eeda38475b5d91a6784170087db9133f2a9b7b07
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-11-11 14:49:14 +0100

    Started with posters

--- src/components/Posters.js
+++ src/components/Posters.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import { collection, query, orderBy, onSnapshot, getDocs } from 'firebase/firestore';
+// import { db } from '../config';
 
 function Posters() {
+    // const colRef = collection(db, 'posters');
+    // const [posters, setPosters] = useState([]);
+
+
+
+    // useEffect(() => {
+    //     const q = query(colRef, orderBy('created', 'desc'))
+    //     onSnapshot(q, (querySnapshot) => {
+    //         setPosters(querySnapshot.docs.map(doc => ({
+    //             id: doc.id,
+    //             data: doc.data()
+    //         })))
+    //     })
+    //     console.log(posters);
+    // }, [])
+
     return (
-        <div>Posters</div>
+        <div>
+            <div>
+                {/* {posters.map((poster) => (
+                    <div key={poster.id}></div>
+                ))} */}
+            </div>
+        </div>
     )
 }
 
