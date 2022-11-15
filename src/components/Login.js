a186226c5409581d5049934eb1cfa35dcb82516c
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-11-15 17:36:08 +0100

    Started on login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,46 @@ import { Link } from 'react-router-dom';
 function Login() {
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
+    const [loginSuccessOrFailureMessage, setLoginSuccessOrFailureMessage] = useState('');
     // const [loginDetails, setLoginDetails] = useState({});
+
+
+
+    const checkIfUserExist = async (loginData) => {
+        try {
+            const response = await fetch('http://localhost:3005/login', {
+                method: 'POST',
+                body: JSON.stringify(loginData),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+
+            if (!response.ok) {
+                if (response.status === 401) {
+                    throw new Error('Email eller lösenord är fel, vänligen försök igen');
+                } else {
+                    throw new Error('Något gick fel men det är inte du - det är vi. Vänligen försök igen senare.');
+                }
+            }
+
+            const data = await response.json();
+            const loggedInUserName = data.firstName;
+
+        } catch (error) {
+            console.log('Login failed', error);
+            setLoginSuccessOrFailureMessage(error.message);
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        const email = loginEmail.trim();
+        const loginData = {
+            email: email,
+            password: loginPassword
+        }
+        checkIfUserExist(loginData);
     }
 
     return (
